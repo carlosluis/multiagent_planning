eaedb248ece072f9771fedcb997741df8a5d007f
Unknown <carlos.luis306@gmail.com>
2018-05-20 10:45:12 -0400

    Third iteration of tuning

--- Tests/comp_confidence.m
+++ Tests/comp_confidence.m
@@ -246,7 +246,7 @@ for q = 1:length(N_vector)
     end
 end
 fprintf("Finished! \n")
-save('comp_confidence_100')
+save('comp_confidence_100_3')
 %% Post-Processing
 
 % Probability of success plots
