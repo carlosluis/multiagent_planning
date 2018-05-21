fd6e81ec4c9c6c322429dc13b490e7e15394be2c
Unknown <carlos.luis306@gmail.com>
2018-05-21 18:31:30 -0400

    Sixth iteration - Bad results

--- Tests/comp_confidence.m
+++ Tests/comp_confidence.m
@@ -246,7 +246,7 @@ for q = 1:length(N_vector)
     end
 end
 fprintf("Finished! \n")
-save('comp_confidence_100_5')
+save('comp_confidence_100_6')
 %% Post-Processing
 
 % Probability of success plots
