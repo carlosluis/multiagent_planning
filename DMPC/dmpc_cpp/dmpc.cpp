3d55edbc8530b84cc977115eb818630a6dfc109e
Unknown <carlos.luis306@gmail.com>
2018-05-22 17:38:04 -0400

    Small bug fix

--- DMPC/dmpc_cpp/dmpc.cpp
+++ DMPC/dmpc_cpp/dmpc.cpp
@@ -416,7 +416,7 @@ Trajectory DMPC::solveQP(const Vector3d &po, const Vector3d &pf,
 
         //NOTE: W *NEEDS* to be different than zero, if not H has determinant 0
 
-        W.block(n_var,n_var,1,1) = pow(10,-10)*(MatrixXd::Identity(1,1));
+        W.block(n_var,n_var,N-1,N-1) = pow(10,0)*(MatrixXd::Identity(N-1,N-1));
 
         a0_1 = VectorXd::Zero(n_var_aug);
         a0_1 << ao, VectorXd::Zero(3*(_k_hor-1) + N - 1);
