30a2d5c5d69cc0139e959457d43a8b6b5e6ba136
Unknown <carlos.luis306@gmail.com>
2018-06-14 18:16:06 -0400

    Fix bug in single agent case

--- DMPC/Matlab/ReachedGoal.m
+++ DMPC/Matlab/ReachedGoal.m
@@ -1,6 +1,12 @@
-function pass = ReachedGoal(p,pf,length_t,error_tol)
+function pass = ReachedGoal(p,pf,length_t,error_tol,N)
 
-differ = squeeze(p(:,length_t,:))- squeeze(pf);
-max_dist = max(sqrt(sum(differ.^2,1)));
+if(N>1)
+    differ = squeeze(p(:,length_t,:))- squeeze(pf);
+    max_dist = max(sqrt(sum(differ.^2,1)));
+else
+    differ = p(:,length_t) - pf';
+    max_dist = max(sqrt(sum(differ.^2,1)));
+end
+    
 
 pass = max_dist < error_tol;
